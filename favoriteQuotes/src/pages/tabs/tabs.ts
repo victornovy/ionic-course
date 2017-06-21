884d67f305d1cb1e170afa86a12b736e9f897837
Novy <novyvictor@gmail.com>
2017-06-21 09:51:29 -0300

    Add tabs navegation

---
+++ favoriteQuotes/src/pages/tabs/tabs.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+import { FavoritesPage } from '../favorites/favorites';
+import { LibraryPage } from '../library/library';
+
+@Component({
+    selector: 'page-tabs',
+    template: `
+        <ion-tabs selectedIndex="1">
+            <ion-tab [root]="favoritesPage" tabTitle="Favorites" ionIcon="star"></ion-tab>
+            <ion-tab [root]="libraryPage" tabTitle="Library" ionIcon="book"></ion-tab>
+        </ion-tabs>
+    `
+})
+
+export class TabsPage {
+    favoritesPage = FavoritesPage;
+    libraryPage = LibraryPage;
+}
