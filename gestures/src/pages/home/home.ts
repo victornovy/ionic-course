633175675093c10f256f069b101df748f6fabd2a
Victor Novy <novyvictor@gmail.com>
2017-07-14 14:53:26 -0300

    Create challenge

--- gestures/src/pages/home/home.ts
+++ gestures/src/pages/home/home.ts
@@ -9,29 +9,44 @@ export class HomePage {
 
     public tapped: number = 0;
     public pressed: number = 0;
+    public won: Boolean = false;
+    public youWon: String = 'U won!';
+    public challenge: String = 'Tap twice, press four times';
 
     constructor(public navCtrl: NavController) { }
 
     tapEvent() {
         this.tapped++;
+        this.checkWon();
     }
 
     pressEvent() {
         this.pressed++;
+        this.checkWon();
     }
 
     resetAll() {
         this.resetTaps();
         this.resetPresses();
+        this.checkWon();
     }
 
     resetTaps() {
         this.tapped = 0;
+        this.checkWon();
     }
 
     resetPresses() {
-        this.pressed = 0  ;
+        this.pressed = 0;
+        this.checkWon();
     }
 
+    checkChallenge() {
+        return this.pressed === 4 && this.tapped === 2;
+    }
+
+    checkWon() {
+        this.won = this.checkChallenge();
+    }
 
 }
